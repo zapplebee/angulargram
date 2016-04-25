4a7d9441504f756b6ec8c7b0b9b8e1106156b622
Zachary Skalko <zskalko@gmail.com>
2016-04-25 00:54:38 -0500

    flip canvas with css

--- static/examples/three.js
+++ static/examples/three.js
@@ -1,3 +1,5 @@
+var truncatedSphere;
+
 var app = angular.module('angulargram',[]);
 app.directive('angulargram', function($http) {
   return {
@@ -18,14 +20,18 @@ app.directive('angulargram', function($http) {
       
       var renderer = new THREE.WebGLRenderer(),
         scene = new THREE.Scene(),
-        camera = new THREE.PerspectiveCamera( 70, e.offsetWidth / e.offsetHeight, 0.1, 1000 ),
+        camera = new THREE.PerspectiveCamera( 70, e.offsetWidth / e.offsetHeight, 0.1, 10 );
+        truncatedSphere = new THREE.Object3D();
         
+        console.log(truncatedSphere.scale.x);
+        scene.add(truncatedSphere);
         
       function initScene(){
         camera.position.z = 0;
         var light = new THREE.PointLight( 0xffffff, .4, 0 );
         light.position.set( 0, 0, 0 );
         scene.add( light );
+        renderer.domElement.style = "-moz-transform: scale(-1, 1);-webkit-transform: scale(-1, 1);-o-transform: scale(-1, 1);transform: scale(-1, 1);filter: FlipH;";
         element.append( renderer.domElement );
       }
       
@@ -61,16 +67,26 @@ app.directive('angulargram', function($http) {
         loadOneTexture(index);
         
         function textureLoaded(texture,index){
+          console.log(texture.image);
           var material = new THREE.MeshPhongMaterial({ map: texture, side: THREE.DoubleSide });
+          //var material = new THREE.MeshPhongMaterial({ color: '#FBA000', side: THREE.DoubleSide });
           var vert = Math.floor(index / 16) + 2;
+          var pane = new THREE.SphereGeometry( 5, 1, 1, (index % 16) * (Math.PI/ 8), Math.PI/ 8, vert * (Math.PI / 8), Math.PI / 8);
+          var mesh = new THREE.Mesh(pane, material);
+
+            //mesh.position.z = mesh.position.z * -1;
+ 
           
-          scene.add(mesh);
+          truncatedSphere.add(mesh);
+          
         }
         
       }
       
       function loop(){
         renderer.setSize( e.offsetWidth, e.offsetHeight );
+        truncatedSphere.rotation.y -= 0.005;
+
         renderer.render( scene, camera );
         window.requestAnimationFrame(loop);
       }
