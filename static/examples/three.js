f4d82dfa276f693c07e48465fd383257263994ea
Zachary Skalko <zskalko@gmail.com>
2016-04-25 02:17:22 -0500

    change light intensity

--- static/examples/three.js
+++ static/examples/three.js
@@ -30,7 +30,7 @@ app.directive('angulargram', function($http) {
         
       function initScene(){
         camera.position.z = 3;
-        var light = new THREE.PointLight( 'rgb(140,140,255)', .7, 0 );
+        var light = new THREE.PointLight( 'rgb(140,140,255)', .2, 0 );
         light.position.set( 0, 0, 0 );
         scene.add( light );
         renderer.domElement.style = "-moz-transform: scale(-1, 1);-webkit-transform: scale(-1, 1);-o-transform: scale(-1, 1);transform: scale(-1, 1);filter: FlipH;";
@@ -94,7 +94,7 @@ app.directive('angulargram', function($http) {
       
       function loop(){
         
-        truncatedSphere.rotation.y -= 0.002;
+        truncatedSphere.rotation.y -= 0.001;
 
         renderer.render( scene, camera );
         window.requestAnimationFrame(loop);
