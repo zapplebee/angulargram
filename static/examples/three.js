f5f4927a019f90680a2c5ae87462bf3c27fc8b59
Zachary Skalko <zskalko@gmail.com>
2016-04-25 20:17:44 -0500

    tidy whitespace

--- static/examples/three.js
+++ static/examples/three.js
@@ -1,13 +1,13 @@
-var app = angular.module('angulargram',[]);
+var app = angular.module('angulargram', []);
 app.directive('angulargram', function($http) {
   return {
 
     restrict: "C",
-    link(scope,element){
+    link(scope, element) {
 
       //main function handles everything except detecting webGL and adding fallback
-      var main = function(){
-      
+      var main = function() {
+
         //declare 
         //get the DOM element from the angular element
         var e = element[0];
@@ -15,60 +15,62 @@ app.directive('angulargram', function($http) {
         var images = [];
         var renderer = new THREE.WebGLRenderer();
         var scene = new THREE.Scene();
-        var camera = new THREE.PerspectiveCamera( 55, window.innerWidth / window.innerHeight, 0.1, 10 );
-          camera.position.z = 3;
+        var camera = new THREE.PerspectiveCamera(55, window.innerWidth / window.innerHeight, 0.1, 10);
+        camera.position.z = 3;
         var truncatedSphere = new THREE.Object3D();
-          scene.add(truncatedSphere);
+        scene.add(truncatedSphere);
 
-        var light = new THREE.PointLight( 'rgb(140,140,255)', .2, 0 );
-          light.position.set( 0, 0, 0 );
-          scene.add( light );
+        var light = new THREE.PointLight('rgb(140,140,255)', .2, 0);
+        light.position.set(0, 0, 0);
+        scene.add(light);
 
-        window.addEventListener('resize',updateSize, false)
+        window.addEventListener('resize', updateSize, false)
 
-        function updateSize(){
+        function updateSize() {
           camera.aspect = window.innerWidth / window.innerHeight;
           camera.updateProjectionMatrix();
-          renderer.setSize( window.innerWidth, window.innerHeight );
+          renderer.setSize(window.innerWidth, window.innerHeight);
         }
 
         updateSize();
-        element.append( renderer.domElement );
+        element.append(renderer.domElement);
 
-        $http({ method: 'GET', url: '/instagram.json?qty=' + qty }).then(getImages, doFallback );
-        
-        function getImages(instagramObject){
+        $http({
+          method: 'GET',
+          url: '/instagram.json?qty=' + qty
+        }).then(getImages, doFallback);
+
+        function getImages(instagramObject) {
           var posts = instagramObject.data;
           //parse the data object from $http and start to load the textures
-          for(var i = 0 ; i < posts.length ; i++){
+          for (var i = 0; i < posts.length; i++) {
             //move new posts to the middle of the array
             images[i % 2 ? "push" : "unshift"](posts[i].images.thumbnail.url)
           }
           doLoadTextures();
         }
 
-
-        function doLoadTextures(){
+        function doLoadTextures() {
 
           var index = 0;
           var textureLoader = new THREE.TextureLoader;
           textureLoader.crossOrigin = "";
 
-          function loadOneTexture(i){
+          function loadOneTexture(i) {
 
             textureLoader.load(
               images[i],
-              function(texture){
-                if(++index < images.length){
+              function(texture) {
+                if (++index < images.length) {
                   loadOneTexture(index);
                 }
-                handleLoadedTexture(texture,i)
+                handleLoadedTexture(texture, i)
 
               },
               function() {
                 //progress
               },
-              function(){
+              function() {
                 doFallback();
               }
             );
@@ -76,7 +78,7 @@ app.directive('angulargram', function($http) {
 
           loadOneTexture(index);
 
-          function handleLoadedTexture(texture,index){
+          function handleLoadedTexture(texture, index) {
 
             //the texture needs to be mirrored horizontally since we see the back side of the sphere.
             //three.js's methods don't seem to work, so make a canvas and do it ourselves.
@@ -92,9 +94,12 @@ app.directive('angulargram', function($http) {
             flippedImg.src = flipCanvas.toDataURL("image/jpg");
             texture.image = flippedImg;
 
-            var material = new THREE.MeshPhongMaterial({ map: texture, side: THREE.BackSide });
+            var material = new THREE.MeshPhongMaterial({
+              map: texture,
+              side: THREE.BackSide
+            });
             var vert = Math.floor(index / 16) + 2;
-            var pane = new THREE.SphereGeometry( 7, 1, 1, (index % 16) * (Math.PI/ 8), Math.PI/ 8, vert * (Math.PI / 8), Math.PI / 8);
+            var pane = new THREE.SphereGeometry(7, 1, 1, (index % 16) * (Math.PI / 8), Math.PI / 8, vert * (Math.PI / 8), Math.PI / 8);
             var mesh = new THREE.Mesh(pane, material);
             truncatedSphere.add(mesh);
 
@@ -102,33 +107,28 @@ app.directive('angulargram', function($http) {
 
         }
 
-        function loop(){
+        function loop() {
           truncatedSphere.rotation.y += 0.001;
-          renderer.render( scene, camera );
+          renderer.render(scene, camera);
           window.requestAnimationFrame(loop);
         }
         loop();
 
+      }
 
-    }
-
-    
       function webglAvailable() {
         try {
           var canvas = document.createElement("canvas");
           return !!
             window.WebGLRenderingContext &&
             (canvas.getContext("webgl") ||
-                canvas.getContext("experimental-webgl"));
-        } catch(e) {
+              canvas.getContext("experimental-webgl"));
+        } catch (e) {
           return false;
         }
       }
 
-
-
-      
-      if(webglAvailable()){
+      if (webglAvailable()) {
         main();
       } else {
         doFallback();
